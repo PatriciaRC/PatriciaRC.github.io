0e0b89b3f4b9535e04b5e878556ffd6fef8f4bd1
PatriciaRC <59322385+PatriciaRC@users.noreply.github.com>
2022-12-09 20:47:12 +0100

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
-import { HomeComponent } from './home/home.component';
+import { AboutComponent } from 'about/about.component';
+import { ContactComponent } from 'contact/contact.component';
+import { HomeComponent } from 'home/home.component';
 
 
 const routes: Routes = [
@@ -21,7 +21,7 @@ const routes: Routes = [
   },
   {
     path:'mywork',
-    loadChildren: () => import('./mywork/mywork.module').then(m => m.MyworkModule)
+    loadChildren: () => import('mywork/mywork.module').then(m => m.MyworkModule)
   }
  
 ];
