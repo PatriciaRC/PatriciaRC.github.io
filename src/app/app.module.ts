194afb31a927ae4bff9e5b0fe0a4f1aca3fad8bb
PatriciaRC <59322385+PatriciaRC@users.noreply.github.com>
2022-12-09 20:46:36 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from 'app-routing.module';
+import { AppComponent } from 'app.component';
+import { HeaderComponent } from 'header/header.component';
+import { FooterComponent } from 'footer/footer.component';
+import { HomeComponent } from 'home/home.component';
+import { ContactComponent } from 'contact/contact.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ContactService } from './contact.service';
-import { AboutComponent } from './about/about.component';
-import { MyworklistComponent } from './myworklist/myworklist.component';
+import { ContactService } from 'contact.service';
+import { AboutComponent } from 'about/about.component';
+import { MyworklistComponent } from 'myworklist/myworklist.component';
 import { initializeApp } from "firebase/app";
 
 @NgModule({
